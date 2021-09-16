2a0400dadbb094b11638ac53c4620d51f3e01407
skiniry <s.kiniry@umail.ucc.ie>
2021-09-16 12:05:08 +0100

    Changed WWW_JS_DIR value

--- mpld3/urls.py
+++ mpld3/urls.py
@@ -11,7 +11,7 @@ import warnings
 __all__ = ["D3_URL", "MPLD3_URL", "MPLD3MIN_URL",
            "D3_LOCAL", "MPLD3_LOCAL", "MPLD3MIN_LOCAL"]
 
-WWW_JS_DIR = "https://mpld3.github.io/js/"
+WWW_JS_DIR = "https://cdn.jsdelivr.net/gh/skiniry/js_test@v1/"
 D3_URL = WWW_JS_DIR + "d3.v3.min.js"
 MPLD3_URL = WWW_JS_DIR + "mpld3.v{0}.js".format(__version__)
 MPLD3MIN_URL = WWW_JS_DIR + "mpld3.v{0}.min.js".format(__version__)
